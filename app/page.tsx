283064fab4d02e1c983d32aebd96f0d2c69cc7b0
Logan M Walsh <loganwalshnz@gmail.com>
2026-07-27 17:42:04 +1200

    Next.js overwrite

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello!</h1>
+      <p>This website is currently under construction. Check back in a few days!</p>
+    </main>
+  )
+}
